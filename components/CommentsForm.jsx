020e24179529d0926034178582c86b26a992fc4c
tqariuqij <thiongojohn91@gmail.com>
2022-03-21 06:24:30 +0300

    detting up the comments form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -10,9 +10,98 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef()
   const storeDataEl = useRef()
 
+  const handleCommentSubmission = () => {
+    setError(false)
+
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+
+    if (storeData) {
+      localStorage.setItem('name', name)
+      localStorage.setItem('email', email)
+    } else {
+      localStorage.removeItem('name', name)
+      localStorage.removeItem('email', email)
+    }
+  }
+
   return (
     <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
-      <h1>post comment form</h1>
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Post comments form
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4 ">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-4 focus:ring-2 focus:ring-gray-200"
+          placeholder="Write your comment here"
+          name="comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2 ">
+        <input
+          type="text"
+          ref={nameEl}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-4 focus:ring-2 focus:ring-gray-200"
+          placeholder="Your name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-4 focus:ring-2 focus:ring-gray-200"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 ">
+        <div>
+          <input
+            ref={storeDataEl}
+            type="checkbox"
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className=" ml-2 cursor-pointer text-gray-500"
+            htmlFor="storeData"
+          >
+            Save my email and name for next time I comment
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className=" ease inline-block cursor-pointer rounded-full bg-pink-500 px-8 py-4 text-lg text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Post comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
     </div>
   )
 }
